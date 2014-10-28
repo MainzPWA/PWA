9e6bc85904b3b60a8faf71f226c2ab2faf220238
Sven Schumann <schumans@kph.uni-mainz.de>
2014-10-28 17:25:17 +0100

    Database update

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-gcc cmake/IncreaseBuild.c -o build/bin/IncreaseBuild
-build/bin/IncreaseBuild src/Build.h
-gcc src/*.cpp -fPIC -pthread -w -O3 -s -march=native -L$ROOTSYS/lib/root -lCore -lCint -lRIO -lNet -lHist -lGraf -lGraf3d -lGpad -lTree -lRint -lPostscript -lMatrix -lPhysics -lMathCore -lThread -lGui -pthread -lm -ldl -rdynamic -lEG -lstdc++ -I$ROOTSYS/include/root -lMinuit -o build/bin/PWA
